58b00336c7a63b26e5c5dfad455f8ffa59d47e3b
Gustavo Guerra <gustavo.guerra@vtex.com.br>
2021-06-25 10:47:38 -0300

    Third commit. Source code should be visible now

---
+++ api-quiz/Models/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace APIQuiz.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public int Score { get; set; }
+        public string Name { get; set; }
+    }
+}
